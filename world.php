a20194debd1c8cd8adccdb6490df4d4b408fda8d
Seb2lyon <seb2lyon71@gmail.com>
2017-04-19 09:18:28 +0200

    Prepare the 2 blocks : 1 French quiz + 1 World quiz

---
+++ world.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Geo-Quizz | Monde</title>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width" />
+	<link rel="stylesheet" type="text/css" href="css/style.css" />
+	<link rel="icon" type="image/x-icon" href="images/favicon.ico" />
+<!--[if IE]><link rel="shortcut icon" type="image/x-icon" href="images/favicon.ico" /><![endif]-->
+</head>
+<body>
+
+<p>En construction</p>
+
+</body>
+</html>
