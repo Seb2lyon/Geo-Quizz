3a78b89959aa8de8990d84a565697c1de3bd0f5a
Seb2lyon <seb2lyon71@gmail.com>
2017-04-20 22:27:36 +0200

    A few modifications...

--- classes/Player.php
+++ classes/Player.php
@@ -3,11 +3,57 @@
 class Player
 {
 	private $_id;
+	private $_shots;
 	private $_loose;
 	private $_win;
 	
-	function __construct(argument)
+	public function __construct(array $datas)
 	{
-		# code...
+		$this->hydrate($datas);
+	}
+
+	public function hydrate($datas)
+	{
+		// Hydratation
+	}
+
+	public function id();
+	{
+		return $this->_id;
+	}
+
+	public function shots();
+	{
+		return $this->_shots;
+	}
+
+	public function loose();
+	{
+		return $this->_loose;
+	}
+
+	public function win();
+	{
+		return $this->_win;
+	}
+
+	public function setId($id);
+	{
+		
+	}
+
+	public function setShots($shots);
+	{
+		
+	}
+
+	public function setLoose($loose);
+	{
+		
+	}
+
+	public function setWin($win);
+	{
+		
 	}
 }
