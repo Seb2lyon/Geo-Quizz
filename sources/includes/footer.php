8f1a9683f399cd48b403a54147cfadb282487bce
Seb2lyon <seb2lyon.info@free.fr>
2020-01-31 18:34:18 +0100

    Update footer.php
    Update version number

--- sources/includes/footer.php
+++ sources/includes/footer.php
@@ -1,6 +1,6 @@
 <footer>
 
-	<p><a href="http://seb2lyon.info.free.fr" target="_blank">Seb2lyon</a>  | version 1.0 - 04.05.2017</p>
+	<p><a href="http://seb2lyon.info.free.fr" target="_blank">Seb2lyon</a>  | version 1.1 - 01.02.2020</p>
 	<p><a href="https://github.com/Seb2lyon/Geo-Quizz" target="_blank">Sources (GitHub)</a> | <a href="https://www.gnu.org/licenses/gpl-3.0-standalone.html" target="_blank">licence GNU v3.0</a></p>
 
 </footer>
